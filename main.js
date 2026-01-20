cd3777042bfa88dbeb3e3f196d564f08c1d470aa
Karan Ram <Karanram73005@gmail.com>
2026-01-21 04:09:22 +0530

    updated style

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-// const toggleBtn = document.getElementById("themeToggle");
-// const html = document.documentElement;
 
-// toggleBtn.addEventListener("click", () => {
-//   const currentTheme = html.getAttribute("data-theme");
-
-//   if (currentTheme === "light") {
-//     html.setAttribute("data-theme", "dark");
-//   } else {
-//     html.setAttribute("data-theme", "light");
-//   }
-// });
 
 
 
